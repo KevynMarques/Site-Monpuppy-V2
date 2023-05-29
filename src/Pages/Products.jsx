7bd21f260be79cad62514b323c3ee662e20e0f8a
Kevyn Marques <kevynmarquesdev@hotmail.com>
2023-05-29 10:01:59 -0200

    correção Header

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -11,17 +11,18 @@ const PageLulu = () => {
   const buttonsWhite = {
     color: '#F5F5F5',
     backgroundColor: 'transparent',
-    fontSize: '14px',
-    lineHeight: '16px',
+    fontSize: '2vh', 
+    lineHeight: '0.741vh',  
   }
   
   const TitleWhite = { 
     color: 'white',
-    fontSize: '22px',
-    marginTop: '8px',
-    lineHeight: '29px',
+    fontSize: '3vh',    
+    marginTop: '0.741vh', 
+    lineHeight: '2.685vh',  
     textAlign: 'center',
   }
+  
   
 
 const StyleHeaderBlack = { ...HeaderBlack}; 
